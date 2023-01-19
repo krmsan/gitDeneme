d52a6c7b40eea523da1c808fc5d468bcb8b2507e
krmsan <sankrmsan@gmail.com>
2023-01-19 18:08:24 +0300

    silindi

--- src/test/java/pages/Student/HomePageSt.java
+++ src/test/java/pages/Student/HomePageSt.java
@@ -2,32 +2,6 @@ package pages.Student;
 
 public class HomePageSt {
 
-    public static void main(String[] args) {
-        System.out.println("abdulkerim");
-    }
-
-
-    public void abdulkerim(){
-
-    }
-
-    public void betul (){
-        System.out.println("deneme");
-    }
-
-
-
-    public void betuull (){
-        System.out.println("deneme2");
-    }
-
-    public void abulkerim22222(){
-        System.out.println("ddffsdfsd");
-
-
-        System.out.println("abdulkerim");
-
-    }
 
 
 
